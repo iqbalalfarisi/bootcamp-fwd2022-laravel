5b3381eaa4d966836e6b4e3e4dff2b38388b26ef
Iqbalalfarisi <iqbalalfarisi07@gmail.com>
2022-11-25 22:52:46 +0700

    fixing relation foreign keys

--- database/migrations/2022_11_24_151419_add_foreign_keys_to_detail_user_table.php
+++ database/migrations/2022_11_24_151419_add_foreign_keys_to_detail_user_table.php
@@ -15,10 +15,10 @@ class AddForeignKeysToDetailUserTable extends Migration
     {
         Schema::table('detail_user', function (Blueprint $table) {
             $table->foreign('user_id', 'fk_detail_user_to_users')
-            ->reference('id')->on('users')->onDelete('CASCADE')
-            ->onUpdate('CASCADE');
+            ->references('id')->on('users')->onUpdate('CASCADE')
+            ->onDelete('CASCADE');
             $table->foreign('type_user_id', 'fk_detail_user_to_type_user')
-            ->reference('id')->on('type_user')->onDelete('CASCADE')->onUpdate('CASCADE');
+            ->references('id')->on('type_user')->onUpdate('CASCADE')->onDelete('CASCADE');
         });
     }
 
