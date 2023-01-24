662063418f8bd4c427c65ff2d358e080d7543f20
Iqbalalfarisi <iqbalalfarisi07@gmail.com>
2023-01-24 22:27:27 +0700

    update login, midleware, providers

--- resources/views/pages/frontsite/landing-page/index.blade.php
+++ resources/views/pages/frontsite/landing-page/index.blade.php
@@ -73,7 +73,71 @@
             </div>
         </section>
         <!-- End Hero -->
+        
+        <!-- Popular Categories -->
+        <section class="mt-32 bg-[#F9FBFC]">
+            <div class="mx-auto max-w-7xl px-4 lg:px-14 py-16">
+                <h3 class="text-2xl font-semibold">Popular Categories</h3>
+                <p class="text-[#A7B0B5] mt-2">Quick way to get your first experience</p>
 
+                <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 sm:gap-8 md:gap-10 lg:gap-12 mt-10">
+                    @forelse($specialist as $key => $specialist_item)
+                        <!-- Card -->
+                            <a href="#" class="bg-white py-6 px-5 rounded-2xl transition hover:ring-offset-2 hover:ring-2 hover:ring-[#0D63F3]">
+                                <h5 class="text-[#1E2B4F] text-lg font-semibold">{{ $specialist_item->name ?? '' }}</h5>
+                                <p class="text-[#AFAEC3] mt-1">143 doctors</p>
+                            </a>
+                        <!-- End Card -->
+                    @empty
+                    {{-- empty --}}
+                    @endforelse
+                </div>
+            </div>
+        </section>
+        <!-- End Popular Categories -->
+
+        <!-- Best Doctors -->
+        <section class="mt-4 lg:mt-16">
+            <div class="mx-auto max-w-7xl px-4 lg:px-14 py-14">
+                <h3 class="text-[#1E2B4F] text-2xl font-semibold">Best Doctors</h3>
+                <p class="text-[#A7B0B5] mt-2">Help your life much better</p>
+
+                <!-- Card -->
+                <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 lg:gap-10 mt-10">
+
+                    @forelse($doctor as $key => $doctor_item)
+
+                        <a href="{{ route('appointment.doctor', $doctor_item->id) }}" class="group">
+                            <div class="relative z-10 w-full h-[350px] rounded-2xl overflow-hidden">
+                                <img src="{{ url(Storage::url($doctor_item->photo)) }}" class="w-full h-full bg-center bg-no-repeat object-cover object-center" alt="{{ $doctor_item->name ?? '' }}">
+                                <div class="opacity-0 group-hover:opacity-100 transition-all ease-in absolute inset-0 bg-[#0D63F3] bg-opacity-70 flex justify-center items-center">
+                                    <span class="text-[#0D63F3] font-medium bg-white rounded-full px-8 py-3">Book Now</span>
+                                </div>
+                            </div>
+
+                            <div class="flex items-center justify-between mt-5">
+                                <div>
+                                    <div class="text-[#1E2B4F] text-lg font-semibold">{{ $doctor_item->name ?? '' }}</div>
+                                    <div class="text-[#AFAEC3] mt-1">{{ $doctor_item->specialist->name ?? '' }}</div>
+                                </div>
+                                <div class="flex items-center space-x-2">
+                                    <img src="{{ asset('/assets/frontsite/images/star.svg') }}" alt="Star">
+                                    <span class="block text-[#1E2B4F] font-medium">4.5</span>
+                                </div>
+                            </div>
+                        </a>
+
+                    @empty
+
+                        {{-- empty --}}
+
+                    @endforelse
+
+                </div>
+                <!-- End Card -->
+            </div>
+        </section>
+        <!-- End Best Doctors -->
 
     </main>
     <!-- End Content -->
